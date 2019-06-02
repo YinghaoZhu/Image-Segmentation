5b095714a3180d5219aa2e9a89ad64ccd5009523
YinghaoZhu <46060763+YinghaoZhu@users.noreply.github.com>
2019-06-02 17:01:56 +1000

    Update data.py

--- data.py
+++ data.py
@@ -31,14 +31,14 @@ def trainGenerator(batch_size, train_path, image_folder, mask_folder, aug_dict, 
     image_datagen = ImageDataGenerator(**aug_dict)
     mask_datagen = ImageDataGenerator(**aug_dict)
     image_generator = image_datagen.flow_from_directory(
-        train_path,  # 训练数据文件夹路径
-        classes=[image_folder],  # 类别文件夹,对哪一个类进行增强
-        class_mode=None,  # 不返回标签
-        color_mode=image_color_mode,  # 灰度，单通道模式
-        target_size=target_size,  # 转换后的目标图片大小
-        batch_size=batch_size,  # 每次产生的（进行转换的）图片张数
-        save_to_dir=save_to_dir,  # 保存的图片路径
-        save_prefix=image_save_prefix,  # 生成图片的前缀，仅当提供save_to_dir时有效
+        train_path, 
+        classes=[image_folder], 
+        class_mode=None,
+        color_mode=image_color_mode,
+        target_size=target_size,
+        batch_size=batch_size,
+        save_to_dir=save_to_dir, 
+        save_prefix=image_save_prefix,
         seed=seed)
     mask_generator = mask_datagen.flow_from_directory(
         train_path,
@@ -50,12 +50,42 @@ def trainGenerator(batch_size, train_path, image_folder, mask_folder, aug_dict, 
         save_to_dir=save_to_dir,
         save_prefix=mask_save_prefix,
         seed=seed)
-    train_generator = zip(image_generator, mask_generator)  # 组合成一个生成器
+    train_generator = zip(image_generator, mask_generator)
     for (img, mask) in train_generator:
-        # 由于batch是2，所以一次返回两张，即img是一个2张灰度图片的数组，[2,256,256]
-        img, mask = adjust_data(img, mask)  # 返回的img依旧是[2,256,256]
+       
+        img, mask = adjust_data(img, mask) 
         yield (img, mask)
-
+# base on test set
+def val_generator(batch_size, train_path, image_folder, mask_folder, aug_dict, image_color_mode="grayscale",
+                   mask_color_mode="grayscale", image_save_prefix="image", mask_save_prefix="mask",
+                   flag_multi_class=False, num_class=2, save_to_dir=None, target_size=(256, 256), seed=1):
+    image_datagen = ImageDataGenerator(**aug_dict)
+    mask_datagen = ImageDataGenerator(**aug_dict)
+    image_generator = image_datagen.flow_from_directory(
+        train_path, 
+        classes=[image_folder],
+        class_mode=None, 
+        color_mode=image_color_mode, 
+        target_size=target_size, 
+        batch_size=batch_size,
+        save_to_dir=save_to_dir, 
+        save_prefix=image_save_prefix,
+        seed=seed)
+    mask_generator = mask_datagen.flow_from_directory(
+        train_path,
+        classes=[mask_folder],
+        class_mode=None,
+        color_mode=mask_color_mode,
+        target_size=target_size,
+        batch_size=batch_size,
+        save_to_dir=save_to_dir,
+        save_prefix=mask_save_prefix,
+        seed=seed)
+    train_generator = zip(image_generator, mask_generator)  
+    for (img, mask) in train_generator:
+        
+        img, mask = adjust_data(img, mask)  # [2,256,256]
+        yield (img, mask)
 
 
 
@@ -67,15 +97,12 @@ def testGenerator(test_path, target_size=(256, 256), as_gray=True):
         img = trans.resize(img, target_size)
         img = img / 255
         img = np.reshape(img, (1,) + img.shape)
-        # 将测试图片扩展一个维度，与训练时的输入[2,256,256]保持一致
+       
         print(img.shape)
         yield img
 
 
 
-
-# # 上面函数是给出测试后的输出之后，为输出涂上不同的颜色，多类情况下才起作用，两类的话无用
-#
 def saveResult(save_path, npyfile, flag_multi_class=False, num_class=2):
 
     for i,item in enumerate(npyfile):
